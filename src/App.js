029eb415f6573a1afcf93a5032ebf7fd3c3e5b3f
Joel Borden <joel.parker714@outlook.com>
2021-03-31 18:09:01 -0400

    testing the slack notification

--- src/App.js
+++ src/App.js
@@ -8,20 +8,6 @@ import Deck from './components/Deck'
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <ErrorBoundary>
         <Deck style={{width: '20rem', height: '100%'}} title="Type of Charts" subtitle="Result"/>
       </ErrorBoundary>
