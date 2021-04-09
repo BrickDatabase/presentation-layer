62f146ccd2e627827e16686e0b5a813baa71752e
Amina Mahmood <37802650+axm6392@users.noreply.github.com>
2021-04-09 00:52:46 -0400

    Added styles for reddit homepage
    Added styles for reddit homepage

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import 'bootstrap/dist/css/bootstrap.css'
 import ErrorBoundary from './components/ErrorBoundary'
 import Chart from './components/Chart'
 import Deck from './components/Deck'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
     <div className="App">
       <ErrorBoundary>
-        <Deck style={{width: '20rem', height: '100%'}} title="Type of Charts" subtitle="Result"/>
+        <Container>
+          <Row>
+            <Col><Deck odp={'inputs'} style={{width: '20rem', height: '25rem', backgroundColor: 'lightgray' }} title="Reddit Topics" subtitle="Result"/></Col>
+            <Col><Deck style={{width: '40rem', height: '25rem', backgroundColor: 'lightgray'}} title="Result" subtitle="Result"/></Col>
+          </Row>
+          <Row>
+            <Col><Deck op={'radios'} style={{width: '20rem', height: '25rem', backgroundColor: 'lightgray' }} title="Type of Charts"/></Col>
+            <Col><Deck op={'fourthBox'} style={{width: '40rem', height: '25rem', backgroundColor: 'lightgray' }} title="Reddit Features" subtitle="Result"/></Col>
+          </Row>
+        </Container>
       </ErrorBoundary>
     </div>
   );
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,8 +2,120 @@ import React, {Component}  from 'react'
 import Card from 'react-bootstrap/Card'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Deck extends React.Component{
+
+    constructor(props) {
+        super(props)
+        
+        switch(this.props.op)
+        {
+            case 'inputs':
+                this.state = {views:this.inputs()}
+                break
+            case 'radios':
+                this.state = {views:this.radios()}
+                break
+            case 'fourthBox':
+                this.state = {views:this.fourthBox()}
+                break
+            default:
+                this.state = {views:this.inputs()}
+                break
+        }
+    }
+
+    inputs()
+        {
+            const v = 
+            <fieldset>
+                <Form.Group as={Row}>
+                <Col sm={12}>
+                    <Form.Check
+                    type="radio"
+                    label="Gifs"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios1"
+                    />
+                    <Form.Check
+                    type="radio"
+                    label="Gaming"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios2"
+                    />
+                    <Form.Check
+                    type="radio"
+                    label="Foodie"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios3"
+                    />
+                    <Form.Check
+                    type="radio"
+                    label="Corgi"
+                    name="formHorizontalRadios"
+                    id="formHorizontalRadios3"
+                    />
+                </Col>
+                </Form.Group>
+            </fieldset>
+
+            return v
+        }
+
+    radios(){
+        const r = 
+        <fieldset>
+        <Form.Group as={Row}>
+          <Col sm={12}>
+            <Form.Check
+              type="radio"
+              label="Bar Chart"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="radio"
+              label="Line Chart"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="radio"
+              label="Pie Chart"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="radio"
+              label="Scatter Chart"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+          </Col>
+        </Form.Group>
+      </fieldset>
+
+      return r
+    }
+
+    fourthBox(){ 
+        const w =
+            <DropdownButton id="dropdown-basic-button" title="Features">
+            <Dropdown.Item href="#/action-1">Comments</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Subscribe</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Views</Dropdown.Item>
+            </DropdownButton>
+
+        return w
+    }
 
 //Type of Charts
 //width: '20rem', height: '100%'
@@ -12,20 +124,14 @@ class Deck extends React.Component{
 //width: '15rem', height: '100%'
     render(){
         return(
-            <div class="card">
-                    <Card style={this.props.style}>
-                        <Card.Body>
+            // <div class="card">
+                <Card style={this.props.style}>
+                    <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">{this.props.subtitle}</Card.Subtitle> 
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                <InputGroup.Radio aria-label="Radio button for following text input" />
-                                </InputGroup.Prepend>
-                                <FormControl aria-label="Text input with radio button" />
-                            </InputGroup>
-                        </Card.Body>
-                    </Card>
-                </div>
+                        {this.state.views}
+                    </Card.Body>
+                </Card>
+            // </div>
         )
     }
 }
